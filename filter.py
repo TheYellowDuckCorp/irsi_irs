76172cd47dd8cdd54c3e7f2d76a614729a868a88
Jorge A. Hoyo García <78392598+a01658142@users.noreply.github.com>
2023-04-10 21:53:33 -0600

    Código completo

--- filter.py
+++ filter.py
@@ -1,21 +1,27 @@
-from scipy.signal import kaiserord, lfilter, firwin, freqz, iirfilter, filtfilt
+from scipy.signal import kaiserord, lfilter, firwin, freqz, iirfilter, filtfilt 
 from scipy.fft import fft, fftfreq
-from scipy.fftpack import fftshift
+from scipy. fftpack import fftshift
 
 def fourier_transform(signal, sample_rate=44100, duration=5):
-    #number of samples
-    N=sample_rate*duration
-    yf=fft(signal)
-    xf=fftfreq(N, 1/sample_rate )
-    #ordered FFT
-    yf=fftshift(yf)
-    yf=fftshift(xf)
+    # Number of samples
+    N = sample_rate * duration
+    yf = fft (signal)
+    xf = fftfreq(N, 1 / sample_rate)
+    # Ordered FFT
+    yf = fftshift(yf)
+    xf = fftshift(xf)
     return xf, yf
 
-def iir_filter(signal, f_cutoff, f_sampling, fbf=false):
-    b, a = iirfilter(4, Wn=f_cutoff, fs=f_sampling, btype="low", ftype="butter")
+def iir_filter(signal, f_cutoff, f_sampling, fbf=False):
+    b, a = iirfilter(4, Wn =f_cutoff, fs = f_sampling, btype="low", ftype="butter")
     if not fbf:
-        filtered = lfilter(b,a,signal)
+        filtered = lfilter(b, a, signal)
     else:
-        filtered = lfilter(b,a,signal)
+        filtered = filtfilt(b, a, signal)
     return filtered
+
+def fir_filter(signal, nyq_rate, f_cutoff):
+    #Window Width
+    width = 5.0/nyq_rate
+    #The desired attenuation in the stop band, in dB
+    ripple_db = 60.0
--- main.py
+++ main.py
@@ -1,26 +1,46 @@
-from filters import fir_filter, iir_filter, fourier_transform
+from filter import fir_filter, iir_filter, fourier_transform
 from scipy.signal import freqz
 from numpy import cos, sin, pi, absolute, arange
 import numpy as np
 from matplotlib import pyplot as plt
+if __name__ == "__main__":
 
-if __name__ "__main__":
-    #create signal
-    np.random.seed(42) #for reproducibility
-    #create time steps and corresponding sine wave with gaussian noise
-    fs=80 #sampling rate HZ
-    ts=np.arange(0,5,1.0 / fs) #timevector - 5sec
-    x_t = np. sin(2*np.pi * 1 * ts) # signal @ 1.0 Hz, without noise
-    noise = 0.2*sin (2*pi*15.3*ts) + 0.1*sin (2*pi *16.7*ts + 0.1) + 0.1*sin (2*pi *23.45 *ts+.8)
+    # Create signal
+    np.random.seed (42) # for reproducibility
+    # create time steps and corresponding sine wave with Gaussian noise
+    fs = 80 # sampling rate, Hz
+    ts = np.arange(0, 5, 1.0 / fs)
+    # time vector - 5 seconds
+    x_t = np.sin(2*np.pi * 1 * ts)
+    # signal @ 1.0 Hz, without noise
+    noise = 0.2*sin (2*pi *15.3*ts) + 0.1*sin(2*pi *16.7*ts + 0.1) + 0.1*sin (2*pi *23.45 *ts+.8)
     #noise = 0.5 * np. random. normal (size-len (ts)) # Gaussian noise
     x_noise = x_t + noise
-
     # plot raw signal
-    plt.figure (figsize= (6.4, 2.4))
+    plt.figure(figsize= (6.4, 2.4))
     plt.plot(ts, x_t, alpha=0.8, lw=3, color="C1", label="Clean signal (ys)")
     plt.plot(ts, x_noise, color="C0", label="Noisy signal (x noise)")
     plt.xlabel("Time / 5")
     plt.ylabel("Amplitude")
-    plt.legend(loc="lower center", bbox_to_anchor= [0.5, 11], ncol=2, fontsize="smaller")
+    plt.legend(loc="lower center", bbox_to_anchor= [0.5, 11], ncol=2,fontsize="smaller")
     plt.tight_layout()
     plt.show()
+    #Show the fourier transform
+    xf, yf = fourier_transform(x_noise, sample_rate=fs,duration=5)
+    plt.plot(xf,np.abs(yf))
+    plt.show()
+    # define lowpass filter with 2.5 Hz cutoff frequency
+    fc = 10
+    x_filtered = iir_filter(x_noise, fc, fs)
+    # Signal Fourier Transform after filtering
+    xf, yf = fourier_transform(x_filtered, sample_rate = fs, duration=5)
+    plt.plot (xf, np.abs (yf))
+    plt.show ()
+    plt.figure(figsize= [6.4, 2.4])
+    plt.plot(ts, x_noise, label="Raw signal")
+    plt.plot (ts, x_filtered, alpha=0.8, lw=3, label="SciPy (filter)")
+    plt.xlabel ("Time / s")
+    plt.ylabel ("Amplitude")
+    plt. legend (loc="lower center", bbox_to_anchor = (0.5, 1), ncol=2,fontsize="smaller")
+    plt.tight_layout()
+    plt.show ()
