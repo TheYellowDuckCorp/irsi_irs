b8ada5b487fed545d8c2b9b4315e3a57ad61d717
Jorge A. Hoyo García <78392598+a01658142@users.noreply.github.com>
2023-04-17 20:39:23 -0600

    filter better
    Added song, audiofilter.py

--- filter.py
+++ filter.py
@@ -1,11 +1,10 @@
 from scipy.signal import kaiserord, lfilter, firwin, freqz, iirfilter, filtfilt 
 from scipy.fft import fft, fftfreq
-from scipy. fftpack import fftshift
-
-def fourier_transform(signal, sample_rate=44100, duration=5):
+from scipy.fftpack import fftshift
+def fourier_transform(signal, sample_rate=44100, duration=5):   
     # Number of samples
     N = sample_rate * duration
-    yf = fft (signal)
+    yf = fft(signal)
     xf = fftfreq(N, 1 / sample_rate)
     # Ordered FFT
     yf = fftshift(yf)
@@ -13,15 +12,26 @@ def fourier_transform(signal, sample_rate=44100, duration=5):
     return xf, yf
 
 def iir_filter(signal, f_cutoff, f_sampling, fbf=False):
-    b, a = iirfilter(4, Wn =f_cutoff, fs = f_sampling, btype="low", ftype="butter")
+    b, a = iirfilter(4, Wn=f_cutoff, fs=f_sampling, btype="low", ftype="butter")
     if not fbf:
         filtered = lfilter(b, a, signal)
     else:
         filtered = filtfilt(b, a, signal)
     return filtered
 
-def fir_filter(signal, nyq_rate, f_cutoff):
-    #Window Width
-    width = 5.0/nyq_rate
-    #The desired attenuation in the stop band, in dB
-    ripple_db = 60.0
+# def fir_filter(signal, nyq_rate, f_cutoff):
+#     #Window width
+#     width = 5.0/nyq_rate
+#     # The desired attenuation in the stop band, in dB.
+#     ripple_db = 60.0
+
+
+
+def fir_filter(signal,nyq_rate, cutoff_hz):
+    width=5.0/nyq_rate
+    ripple_db=10.0
+    N,beta=kaiserord(ripple_db,width)
+    taps=firwin(N, cutoff_hz/nyq_rate,window=("kaiser",beta))
+    filtered_x = lfilter(taps,1.0,signal)
+
+    return filtered_x, taps, N
